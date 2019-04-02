b1a78215187b966e8523d4e780032898f2a5e4b1
Tan <xuantan97@gmail.com>
2019-04-02 23:32:58 +0700

    init pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import Signup from './User/Signup';
+import Login from './User/Login';
+import Dashboard from './Dashboard/Dashboard';
+import { PrivateRoute } from './PrivateRoute';
+import { createBrowserHistory } from 'history';
+// import './App.css';
+
+const history = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="col-sm-12">
+        <Router history={history}>
+          <div>
+            <ul>
+              <div>
+                <li><Link to="/">Trang chủ</Link></li>
+                <li><Link to="/live">Cấu hình stream</Link></li>
+              </div>
+              <li style={{ float: 'right' }}><Link to="/login">Đăng xuất</Link></li>
+            </ul>
+            <div className="col-sm-8 col-sm-offset-2">
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                {/* <Route component={NotFoundPage} /> */}
+              </Switch>
+            </div>
+          </div>
+        </Router >
+      </div >
     );
   }
 }
