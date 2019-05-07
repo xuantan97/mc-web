1452b30bc348cd52a24beb94bd8765f23864aae1
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-07 20:46:33 +0700

    update code test

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -71,6 +71,74 @@ export default class Dashboard extends React.Component {
         .catch(error => console.log(error));
     }
 
+    Todo1() {
+        fetch('http://bonddemo.tk/v1/question/render-question?difficulty=3', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+                'Content-Type': 'text/plain'
+            },
+        })
+        .then(res => res.json())
+        .then(response => {
+            console.log(response);
+           
+        })
+        .catch(error => console.log(error));
+    }
+
+
+    Todo2() {
+        fetch('http://bonddemo.tk/v1/question/request-question-node', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+                'Content-Type': 'text/plain'
+            },
+        })
+        .then(res => res.json())
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error));
+    }
+
+    Todo3() {
+        var form = new FormData();
+        form.append('email', 'user@example.com');
+        form.append('answer', 'A');
+
+        fetch('http://bonddemo.tk/v1/question/check-answer', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+                'Content-Type': 'text/plain'
+            },
+            body: form
+        })
+        .then(res => {
+            res.json().then(response => {
+                console.log(response);
+            })
+        })
+        .catch(error => console.log(error));
+    }
+
+
+    Todo4() {
+        fetch('http://bonddemo.tk/v1/question/summary-question', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
+                    'Content-Type': 'text/plain'
+                },
+            })
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => console.log(error));
+    }
 
 
     render() {
@@ -78,6 +146,10 @@ export default class Dashboard extends React.Component {
             <div>
                 <WebRTCVideo></WebRTCVideo>
                 <Button onClick={() => this.getQuestion()}>Get Question</Button>
+                <Button onClick={() => this.Todo1()}>Test 1</Button>
+                <Button onClick={() => this.Todo2()}>Test 2</Button>
+                <Button onClick={() => this.Todo3()}>Test 3</Button>
+                <Button onClick={() => this.Todo4()}>Test 4</Button>
                 <div id="question-area"></div>
             </div>
         );
