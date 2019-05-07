6b5038dab7bcc5d1c28e2dd51aa1f61af8f18d90
Phan Văn Tấn <tanphan0805@gmail.com>
2019-05-07 23:08:07 +0700

    add button getQuestionClient

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -9,18 +9,17 @@ export default class Dashboard extends React.Component {
         super(props);
         this.state = { 
             value: '' ,
-            endpoint: "localhost:1235"
         };
 
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    socket = io("103.89.85.105:1235");
 
     componentDidMount() {
-        const socket = io(this.state.endpoint);
 
-        socket.on('CLOSE_QUESTION', () => {
+        this.socket.on('CLOSE_QUESTION', () => {
 
             fetch('http://bonddemo.tk/v1/question/summary-question', {
                 method: 'GET',
@@ -47,10 +46,11 @@ export default class Dashboard extends React.Component {
         event.preventDefault();
     }
 
-    getQuestion() {
-        const socket = io(this.state.endpoint);
+    startGame() {
+        this.socket.emit("START_GAME");
+    }
 
-       // var str = "";
+    getQuestionMC() {
         fetch('http://bonddemo.tk/v1/question/render-question?difficulty=3', {
             method: 'GET',
             headers: {
@@ -60,97 +60,41 @@ export default class Dashboard extends React.Component {
         })
         .then(res => res.json())
         .then(response => {
+            response.body = JSON.parse(response.body);
+            
             console.log(response);
-            //response.body = JSON.parse(response.body);
-           // str.concat(response.title);
             $('#question-area').html(response.title);
+            $('#answer-A-area').html("A. " + response.body.A );
+            $('#answer-B-area').html("B. " + response.body.B );
+            $('#answer-C-area').html("C. " + response.body.C );
+            $('#correct-answer-area').html("Correct: " + response.answer);
 
-            //emit question to server node
-            socket.emit('GO_TO_GET_QUESTION');
-        })
-        .catch(error => console.log(error));
-    }
-
-    Todo1() {
-        fetch('http://bonddemo.tk/v1/question/render-question?difficulty=3', {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-                'Content-Type': 'text/plain'
-            },
-        })
-        .then(res => res.json())
-        .then(response => {
-            console.log(response);
-           
         })
         .catch(error => console.log(error));
     }
 
 
-    Todo2() {
-        fetch('http://bonddemo.tk/v1/question/request-question-node', {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-                'Content-Type': 'text/plain'
-            },
-        })
-        .then(res => res.json())
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => console.log(error));
+    getQuestionClient() {
+        //emit question to server node
+        this.socket.emit('GO_TO_GET_QUESTION');
     }
 
-    Todo3() {
-        var form = new FormData();
-        form.append('email', 'user@example.com');
-        form.append('answer', 'A');
-
-        fetch('http://bonddemo.tk/v1/question/check-answer', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-                'Content-Type': 'text/plain'
-            },
-            body: form
-        })
-        .then(res => {
-            res.json().then(response => {
-                console.log(response);
-            })
-        })
-        .catch(error => console.log(error));
-    }
-
-
-    Todo4() {
-        fetch('http://bonddemo.tk/v1/question/summary-question', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer lyWyy7-2EqXt6JOjKXnQV90Ghv94ie_5vO20rHFP',
-                    'Content-Type': 'text/plain'
-                },
-            })
-            .then(res => res.json())
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => console.log(error));
-    }
+   
 
 
     render() {
         return (
             <div>
                 <WebRTCVideo></WebRTCVideo>
-                <Button onClick={() => this.getQuestion()}>Get Question</Button>
-                <Button onClick={() => this.Todo1()}>Test 1</Button>
-                <Button onClick={() => this.Todo2()}>Test 2</Button>
-                <Button onClick={() => this.Todo3()}>Test 3</Button>
-                <Button onClick={() => this.Todo4()}>Test 4</Button>
+                <Button onClick={() => this.startGame()}>Start Game</Button>
+                <Button onClick={() => this.getQuestionMC()}>Get Question MC</Button>
+                <Button onClick={() => this.getQuestionClient()}>Get Question Client</Button>
+               
                 <div id="question-area"></div>
+                <div id="answer-A-area"></div>
+                <div id="answer-B-area"></div>
+                <div id="answer-C-area"></div>
+                <div id="correct-answer-area"></div>
             </div>
         );
     }
